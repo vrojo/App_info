abfda1012ec589f9a65391b25641c230d0b6a35a
olivierBatier <olivierbatier@gmail.com>
2015-12-26 16:35:23 +0100

    modif carte

--- Eventmap.js
+++ Eventmap.js
@@ -2,40 +2,44 @@ var geocoder;
 var map;
 
 
-function Eventmap()
+function Eventmap(listeadresse, lieu)
 {
 geocoder = new google.maps.Geocoder();
-var address = "Paris";
 var latlng = new google.maps.LatLng(0,0);
 var mapOptions = 
 	{
-		zoom: 15,
+		zoom: 12,
 		center: latlng
 	}
 
 map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
-  google.maps.event.addDomListener(map, 'click', function() {
+  /*google.maps.event.addDomListener(map, 'click', function() {
     window.alert('Map was clicked!');
-  });
+  });*/
   
-  	geocoder.geocode( { 'address': address}, function(results, status) 
+  	geocoder.geocode( { 'address': lieu}, function(results, status) 
 	{
 		if (status == google.maps.GeocoderStatus.OK) 
 		{
 			map.setCenter(results[0].geometry.location);
-			var marker = new google.maps.Marker(
-			{
-				map: map,
-				position: results[0].geometry.location,
-				title: 'Hello World!',
-				animation: google.maps.Animation.DROP,
-			});
-		google.maps.event.addListener(marker,'click',function() {
-		map.setZoom(9);
-		map.setCenter(marker.getPosition());
+			for(var i=0; i<listeadresse.length;i++){	
+				geocoder.geocode( { 'address': listeadresse[i]}, function(results2, status){
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results2[0].geometry.location,
+						title: 'Hello World!',
+						animation: google.maps.Animation.DROP,
+					});
+				google.maps.event.addListener(marker,'click',function() {
+			map.setZoom(15);
+			map.setCenter(marker.getPosition());
   });
+				})
+				
+			}
+			
 		} 
 	else {
 	alert("Geocode was not successful for the following reason: " + status);
