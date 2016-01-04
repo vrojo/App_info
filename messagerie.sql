486437e1f7159a440ff86f9f9d0f290123e3cf2e
AntoineISEP <ntnlatour@gmail.com>
2016-01-04 13:49:13 +0100

    Messagerie BDD
    Messagerie BDD

---
+++ messagerie.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 04 Janvier 2016 à 13:47
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `bddsimplevent`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messagerie`
+--
+
+CREATE TABLE IF NOT EXISTS `messagerie` (
+  `id_message` int(11) NOT NULL AUTO_INCREMENT,
+  `sujet` text NOT NULL,
+  `texte` text NOT NULL,
+  `id_destinataire` int(11) NOT NULL,
+  `id_expediteur` int(11) NOT NULL,
+  `nom_destinataire` varchar(61) NOT NULL,
+  `nom_expediteur` varchar(61) NOT NULL,
+  PRIMARY KEY (`id_message`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
