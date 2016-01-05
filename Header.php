2a12ca8ca0dddec5146ad15a84de0234f1967654
olivierBatier <olivierbatier@gmail.com>
2016-01-05 17:36:42 +0100

    Wololo4

--- Header.php
+++ Header.php
@@ -12,8 +12,8 @@ require'fonctions_simplevent.php'; ?>
 		<div id="headerdroit">
 			<?php if(isset($Accueil)==FALSE) { ?>				
 			<div class="bandeauhaut">
-				<form method="post" action="">
-					<input type="search" name="recherche" id="recherche" placeholder="Ex:Tommorrow Land, Rock, Football..."/> 
+				<form method="post" action="Evenements2.php">
+					<input type="search" name="mot_clef" name="ville_evenement" id="recherche" placeholder="Ex:Tommorrow Land, Rock, Football..."/> 
 					<input type="submit" class="boutonrecherche" value="Recherche d'événements"/>
 				</form>
 			</div>
@@ -32,13 +32,13 @@ require'fonctions_simplevent.php'; ?>
 			<div class="Boutonmenu">
 				<a href="Events.php?Event_id=1" style="color:inherit"><p>Evénements</p></a>
 				<div class="menuderoul" >
-					<a href="#" style="color:inherit"><div class="bandeaubas" style="height:33%">
+					<a href="Evenements2.php?t=Eventscrees" style="color:inherit"><div class="bandeaubas" style="height:33%">
 						<p style="display:inline-block; float:left; position:absolute;left:5%">Evénements créés</p>
 					</div></a>
-					<a href="#" style="color:inherit"><div class="bandeaubas" style="height:33%;">
+					<a href="Evenements2.php?t=MesEvents" style="color:inherit"><div class="bandeaubas" style="height:33%;">
 						<p style="display:inline-block; float:left; position:absolute;left:5%">Vos Evénements</p>
 					</div></a>
-					<a href="#" style="color:inherit;"><div class="bandeaubas" style="height:33%">
+					<a href="Evenements2.php" style="color:inherit;"><div class="bandeaubas" style="height:33%">
 						<p style="display:inline-block; float:left; position:absolute;left:5%">Evénements intéressants</p>
 					</div></a>
 				</div>	
