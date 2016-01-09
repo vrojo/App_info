08029fa8954de9c7f4c58199cf7cda194af722a5
vrojo <vin.rojo@gmail.com>
2016-01-09 15:09:34 +0100

    mise à jour de la creation d'event
    il faut ajouter  les post de l'adresse de l'event et gerer les liaisons
    avec multimedia, typeevent sur la bdd. dans eventcree.php et  fonction
    crea event

--- Vue/creationevent.php
+++ Vue/creationevent.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'fonctions_crea_event.php';
+require '../Modele/fonctions_crea_event.php';
 ?>
 <!DOCTYPE html>
 <!--
@@ -22,15 +22,7 @@ and open the template in the editor.
 
             <table align="center" id="table1">
             
-                <td><h3>Vous voulez inviter les gens à une dégustation...</h3>
-                    <img id="picnic" src="http://www.tourisme-sancerre.com/contenus/95/cms_entite/608/150423170105_degustation-dans-une-cave-de-sancerre.jpg" border="3"/><br>
-                    <h3>...ou à un cours de danse à l'extérieur?</h3>
-                    <img id="picnic" src="http://www.clicanoo.re/IMG/jpg/EST-gym-plein-air-20151112_175331_JIR_731783.jpg" border=3 />
-                </td>
-            
-            
-                
-                <td><em><h1>Créer un événement:</h1><br>*créez rapidement un événement privé ou visible par tous en remplissant ce formulaire</em><br>&nbsp;
+                <td align='center'><em><h1>Créer un événement:</h1><br>*créez rapidement un événement privé ou visible par tous en remplissant ce formulaire</em><br>&nbsp;
                     <table>
                 
                         <tr>
@@ -43,7 +35,7 @@ and open the template in the editor.
                         <tr>
             
                         <td height=25 align="center"><label for="login"><strong>Adresse :</strong></label></td>
-                        <td align="left"><input type="text" name="lieu" id="adresseevent" placeholder="Place de la Bastille"/></td>
+                        <td align="left"><input type="text" name="lieu" id="adresseevent1" placeholder="numéro"/> &nbsp; <input type="text" name="lieu" id="adresseevent3" placeholder="rue"/> &nbsp; <input type="text" name="lieu" id="adresseevent1" placeholder="ville"/> &nbsp; <input type="text" name="lieu" id="adresseevent2" placeholder="code postal"/> &nbsp; <input type="text" name="lieu" id="adresseevent1" placeholder="pays"/></td>
             
                         </tr>
                         <td></td>
@@ -52,28 +44,38 @@ and open the template in the editor.
                         <td height=25 align="center"><label for="login"><strong>Prix :</strong></label></td>
                         <td align="left"><input type="number" name="prix" id="prix" placeholder="10€"/></td>
             
-                    </tr>            
-                    <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan='2' align='center'>_______________________</td>
+                    </tr>
+                    <td colspan='2' align='center'>_________________________________________________________________________________________</td>
+                    <tr>
+                        <td colspan='2' align='center'>_______________________<br><br><br>&nbsp;</td>
+                    </tr>
                     <tr>
             
-                    <td height=25 align="center"><label for="pass"><strong>Date de l'événement :</strong></label></td>
-                    <td align="left"><input type="date" name="date_e" id="date_event" placeholder="jour"/></td>
+                    <td height=25 align="center" colspan='2'><label for="pass"><strong>Date de l'événement :</strong></label></td>
+                    </tr>
+                    <tr>
+                    <td align="right">début:<br><input type="date" name="date_e" id="date_event"/></td>
+                    <td align="center">fin:<br><input type="date" name="date_f" id="date_event"/></td>
                     
                     </tr>
-                    <td></td>
+                    <td>&nbsp;<br>&nbsp;</td>
                     <tr>
             
-                    <td height=25 align="center"><label for="pass2"><strong>horaires :</strong></label></td>
-                    <td align="left">début: &nbsp;<input type="time" name="heuredebut" id="heureevent" placeholder="heure"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; fin: &nbsp;&nbsp;<input type="time" name="heurefin" id="heureevent" placeholder="heure"/></td>
+                    <td height=25 align="center" colspan='2'><label for="pass2"><strong>horaires :</strong></label></td>
                     </tr>
-                    <td></td>
+                    <tr>
+                    <td align="center" colspan='2'>début: &nbsp;<input type="time" name="heuredebut" id="heureevent" placeholder="heure"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; fin: &nbsp;&nbsp;<input type="time" name="heurefin" id="heureevent" placeholder="heure"/></td>
+                    </tr>
+                    <td>&nbsp;<br>&nbsp;</td>
                     <tr>
             
-                    <td height=25 align="center"><label for="pass"><strong>Nombre maximum de participants :</strong></label></td>
-                    <td align="left"><input type="number" name="nb_max_participant" id="nombre" placeholder="25"/></td>
+                    <td height=25 align="center" colspan='2'><label for="pass"><strong>Nombre maximum de participants :</strong></label>&nbsp;&nbsp;&nbsp;<input type="number" name="nb_max_participant" id="nombre" placeholder="25"/></td>
                     
                     </tr>
-                    <td></td>
+                    <td>&nbsp;<br>&nbsp;</td>
                     <tr>
             
                     <td height=25 align="center"><label for="login"><strong>Catégorie de l'event :</strong></label></td>
@@ -92,35 +94,42 @@ and open the template in the editor.
                                        <!--fin du test--></td></tr></table></td>
             
                     </tr>
+                    </tr>
+                    <td>&nbsp;<br>&nbsp;</td>
                     <tr>
-                    <td height=25 align="center"><label for="login"><strong>Description :</strong><br><br><em>*Ecrivez une petite des-<br> cription de l'événement<br>qui pourra être vue par<br>les autres utilisateurs sur <br> la page de l'événement.</em></label></td>
+                    <tr>
+                    <td height=25 align="center"><label for="login"><strong>Description :</strong><br><br><em>*Ecrivez une petite description de l'événement<br>qui pourra être vue par les autres utilisateurs sur <br> la page de l'événement.</em></label></td>
                     <td align="left">&nbsp;&nbsp;<textarea name="description_e" id="description" placeholder="entrez une petite description de l'événement pour inciter les autres utilisateurs à s'inscrire !"></textarea></td>
                     </tr>
                     <td>&nbsp;</td>
                     <tr></tr>
                     <td>&nbsp;</td>
                     <tr>
-                    <td style="border:1px dashed black">&nbsp;<em>**Créez un événement privé pour lequel <br>vous pourrez désigner les personnes ayant la <br>possibilité de voir l'événement sur le site</em></td><td align="left"><INPUT type="checkbox" name="privacy" value="10"> <strong>événement privé</strong>
+                    <td align="right" >&nbsp;<em>**Créez un événement privé pour lequel <br>vous pourrez désigner les personnes ayant la <br>possibilité de voir l'événement sur le site</em></td><td id="prive" align="left"><INPUT type="checkbox" name="privacy" value="10"> <strong>événement privé</strong>
                 
                     </td>
                     </tr>
-                
-               
-                    <tr>
                     
-                    <table cellspacing="50">
-                        <tr><td><img id="bouton3" style="color:black"><div id="go3"><strong><u><em>Ajouter une image /<br> une vidéo / un lien</em></u></strong></div></a></td>
-                        <td></td>
-                        <td align="left">url d'images<br><input type="text" name="urlimg_event" id="adresseevent" placeholder=""/><img id="plus" src="http://www.icone-png.com/png/20/20012.png"/><br>url de vidéos<br><input type="text" name="urlvid_event" id="adresseevent" placeholder=""/><img id="plus" src="http://www.icone-png.com/png/20/20012.png"/><br>url du site<br><input type="text" name="urlsite_event" id="adresseevent" placeholder=""/><img id="plus" src="http://www.icone-png.com/png/20/20012.png"/></td>
-                        </tr>
-                        <tr><td colspan="3" align="right"><a href="BDDevent.php" id="bouton" style="color:black"><strong><u><em><input type="submit" value="créer" id="go"/></em></u></strong></a></td></tr>
-                    </table>
+                    
+                    
+                    
+                    
+                    
+                    
+                    <tr>
+                        <table cellspacing="50">
+                            <tr>
+                                <td align="left">url d'image<br><input type="text" name="urlimg_event" id="adresseevent" placeholder=""/><img id="plus" src="http://www.icone-png.com/png/20/20012.png"/></td><td>url du site<br><input type="text" name="urlsite_event" id="adresseevent" placeholder=""/><img id="plus" src="http://www.icone-png.com/png/20/20012.png"/></td>
+                            </tr>
+                            <tr>
+                                <td id="creer" colspan="2" align="right"><a href="BDDevent.php" id="bouton" style="color:black"><strong><u><em><input type="submit" value="créer" id="go"/></em></u></strong></a></td>
+                            </tr>
+                        </table>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><?php include '../Vue/footer.php';?></td>
                     </tr>
                     
-                </table>
-            </td>
-
-            </table>     
-            </form>
+                </table>    
     </body>
 </html>
