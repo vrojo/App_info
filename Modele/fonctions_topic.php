555c7caff2cdd0ae5691f7075645dba5b476fac3
olivierBatier <olivierbatier@gmail.com>
2016-01-14 22:17:47 +0100

    topic

--- Modele/fonctions_topic.php
+++ Modele/fonctions_topic.php
@@ -4,8 +4,7 @@ mysqli_set_charset($connect,"utf8");
 function coms ($Topic_id){
 	global $connect;
 	global $id_utilisateur;
-	$result=mysqli_query($connect,"SELECT * from reponse where sujet=$Topic_id");
-	
+	$result=mysqli_query($connect,"SELECT * from reponse where sujet='$Topic_id'");
 while ($data = mysqli_fetch_assoc($result)) {
 	$id_commentateur=$data['id_utilisateur'];
 	$util=mysqli_fetch_assoc(mysqli_query($connect,"SELECT * from utilisateur where id_utilisateur=$id_commentateur"));
@@ -28,7 +27,7 @@ while ($data = mysqli_fetch_assoc($result)) {
 				</div>
 			</div>
 			<div class="bleft" style="width:50%; height:auto; min-height:125px">
-				<p style="position:relative;display:inline-block;height:auto;width:100%;margin: 0;margin-top:10px; text-align:left;word-wrap: break-word;"><?php echo $data['comentaire_r']; ?> </p>
+				<p style="position:relative;display:inline-block;height:auto;width:100%;margin: 0;margin-top:10px; text-align:left;word-wrap: break-word;"><?php echo $data['commentaire_r']; ?> </p>
 			</div>
 			<div class="bright" style="width:20%; height:125px;">
 				<?php 
--- Vue/Topic.php
+++ Vue/Topic.php
@@ -9,9 +9,7 @@ $id_utilisateur=$_SESSION['id_utilisateur']
 		<meta charset="utf-8" />
 		<link type="text/css" rel="stylesheet" href="../Style/Topic.css"/>
 		<script type="text/javascript" src="../Modele/Eventmap.js"></script>
-		<title>Forum</title>
-		
-		
+		<title>Forum SimplEvent</title>		
 	</head>
 	
 	<body>
@@ -20,9 +18,24 @@ $id_utilisateur=$_SESSION['id_utilisateur']
 		require 'fonctions_topic.php'; 
 		?>
 		<div id="Corps_forum">
-			<?php 
-				coms($_GET['Topic'])
-			?>
+			<div id="sujet">
+				
+			</div>
+			<div id="bandeau2_1" style="height:100px">
+				<a href=#bandeau2_2><div class="Bouton2" style="width:50%;height:50px"><p>Ajouter un commentaire</p></div></a>
+			</div>
+			<div id="bandeau2" style="height:auto">
+				<?php 
+					coms($_GET['Topic'])
+				?>
+			</div>
+			<div id="bandeau2_2" >
+				<form method="post" style="height:100%" action="../Controler/addcom.php">
+					<textarea name="Commentaire" placeholder="Entrez votre commentaire" id="Zonecom"></textarea>
+					<input type="hidden" name="Topic" value= <?php echo $_GET['Topic'] ?> />
+					<input type="submit" name="envoyer" value="Envoyer" id="Boutoncom"/>
+				</form>
+			</div>
 		</div>
 	</body>
 	</head>
