77b1c984f3b675a51edd963e2164461f843efce5
poccoss <pierreolivier.carli@gmail.com>
2016-01-15 16:14:06 +0100

    have fun

--- Modele/eventcree.php
+++ Modele/eventcree.php
@@ -1,24 +1,24 @@
-
+<?php
+session_start();
+?>
 
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Evénement créé</title>
     </head>
     <body>
-        <link type="text/css" rel="stylesheet" href="Header.css"/>
-       <?php include ("Header.php"); ?>
-        <?php
-require 'fonctions_crea_event.php';
-insert_event($_POST['Nom_e'], $_POST['lieu'], $_POST['prix'], $_POST['date_e'], $_POST['heuredebut'], $_POST['heurefin'], $_POST['nb_max_participant'], $_POST['description_e'], $_POST['privacy'],$_SESSION['id_utilisateur']);
-
-?>
-        <?php 
-        header("Refresh:0, url = Accueil.php");
-        exit();
+		<?php include ("Header.php");
+		//$urlsite
+		require 'fonctions_crea_event.php';
+		insert_event($_POST['sponsor1'], $_POST['sponsor2'], $_POST['sponsor3'], $_POST['sponsor4'], $_POST['codepostal'], $_POST['numerorue'], 
+		$_POST['pays'], $_POST['rue'], $_POST['ville'], $_POST['date_e'], $_POST['date_f'], $_POST['description_e'], $_POST['heuredebut'], 
+		$_POST['heurefin'], $_POST['nb_max_participant'], $_POST['Nom_e'], $_POST['privacy'], $_POST['prix'], $_POST['photo_principale'], 
+		$_POST['photo_secondaire'], $_POST['photo_trois'], $_POST['photo_quatre'], $_POST['urlsite_event']);
+ 
+        header("Refresh:5, location=Accueil.php");
         ?>
-        
     </body>
 </html>
