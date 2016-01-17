2363592c85b3f3809199ee323402c0dc7f88a885
poccoss <pierreolivier.carli@gmail.com>
2016-01-17 15:42:36 +0100

    dernier

--- Vue/user.php
+++ Vue/user.php
@@ -2,11 +2,6 @@
 require '../Controler/fonction_user.php';
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <title>profiluser</title>
