30c6765772183d8a03c8bbbe8e1082c30b73df34
AntoineISEP <ntnlatour@gmail.com>
2016-01-17 17:30:23 +0100

    Commentaires

--- Modele/fonction_sujet.php
+++ Modele/fonction_sujet.php
@@ -1,5 +1,7 @@
 <?php
 include 'model.php';
+//Fonction pour insérer un sujet dans la base de données
+
 function insert_sujet($sujet, $message, $id_utilisateur){
     global $connect;
     $sujete=htmlspecialchars (addslashes($sujet));
@@ -7,7 +9,7 @@ function insert_sujet($sujet, $message, $id_utilisateur){
     $req= "INSERT INTO sujet (sujet, text_s, id_utilisateur, date_s) values ('$sujete', '$messagee', '$id_utilisateur', NOW())";
     mysqli_query($connect, $req) or die("MySQL Erreur : " . mysqli_error($connect));
 }
-
+//Fonction pour insérer une réponse dans la table appropriée
 function insert_reponse($sujete, $id_utilisateur){
     global $connect;
     $req2= "INSERT INTO rep_topic (id_topic, commentaire_r, id_utilisateur , date) values ('$sujete', 0, '$id_utilisateur', NOW() )";
